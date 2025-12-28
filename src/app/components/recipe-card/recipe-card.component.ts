d600d03b24f0b0323cd773687d392c3e2a5340f5
artem-totality <artem.totality@gmail.com>
2025-12-28 23:32:35 +0000

    Add recipe card buttons & adjust layout

--- src/app/components/recipe-card/recipe-card.component.ts
+++ src/app/components/recipe-card/recipe-card.component.ts
@@ -1,15 +1,21 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { IonButton, IonIcon } from '@ionic/angular/standalone';
 import { RecipeModel } from 'src/app/models/recipe.model';
+import { addIcons } from 'ionicons';
+import { heart } from 'ionicons/icons';
 
 @Component({
   selector: 'recipe-card',
   templateUrl: './recipe-card.component.html',
   styleUrls: ['./recipe-card.component.scss'],
+  imports: [IonButton, IonIcon],
 })
 export class RecipeCardComponent implements OnInit {
   @Input()
   recipe!: RecipeModel;
-  constructor() {}
+  constructor() {
+    addIcons({ heart });
+  }
 
   ngOnInit() {}
 }
