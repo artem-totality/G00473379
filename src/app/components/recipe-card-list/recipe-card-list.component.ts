e85365ba5214bc15889b11244dadbc44e8691ced
artem-totality <artem.totality@gmail.com>
2025-12-28 12:52:21 +0000

    Add recipe-card & recipe-card-list starters

---
+++ src/app/components/recipe-card-list/recipe-card-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'recipe-card-list',
+  templateUrl: './recipe-card-list.component.html',
+  styleUrls: ['./recipe-card-list.component.scss'],
+})
+export class RecipeCardListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/app/components/recipe-card/recipe-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'recipe-card',
+  templateUrl: './recipe-card.component.html',
+  styleUrls: ['./recipe-card.component.scss'],
+})
+export class RecipeCardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
