82160c6994650189e0ce8a818ae6ed9d9d13e1e2
artem-totality <artem.totality@gmail.com>
2025-12-28 13:01:13 +0000

    Add reciept-list to main page

--- src/app/components/recipe-card-list/recipe-card-list.component.ts
+++ src/app/components/recipe-card-list/recipe-card-list.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeCardComponent } from '../recipe-card/recipe-card.component';
 
 @Component({
   selector: 'recipe-card-list',
   templateUrl: './recipe-card-list.component.html',
   styleUrls: ['./recipe-card-list.component.scss'],
+  imports: [RecipeCardComponent],
 })
 export class RecipeCardListComponent implements OnInit {
   constructor() {}
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from '@ionic/angular/standalone';
+import { RecipeCardListComponent } from 'src/app/components/recipe-card-list/recipe-card-list.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    RecipeCardListComponent,
+  ],
 })
 export class HomePage {
   constructor() {}
