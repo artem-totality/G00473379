77f0b0d36c8be9a707e4a270e01a1de503a5adee
artem-totality <artem.totality@gmail.com>
2025-12-29 20:55:51 +0000

    Add Header navigation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonButton,
+  IonButtons,
+  IonIcon,
+} from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { optionsOutline, heart } from 'ionicons/icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle],
+  imports: [IonHeader, IonToolbar, IonTitle, IonIcon, IonButton, IonButtons],
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {
+    addIcons({ heart, optionsOutline });
+  }
 
   ngOnInit() {}
+
+  goHome() {
+    this.router.navigate(['/home']);
+  }
+
+  goFavorite() {
+    this.router.navigate(['/favorite']);
+  }
+
+  goSettings() {
+    this.router.navigate(['/settings']);
+  }
 }
